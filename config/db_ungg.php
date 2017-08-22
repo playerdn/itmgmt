155a6ec32351554e0d95b6440e93bb43853cc09e
Andrey Shevchik <shevchyk.aa@gmail.com>
2017-08-22 17:30:12 +0300

    Testing gitignore

--- config/db_ungg.php
+++ config/db_ungg.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=127.0.0.1;dbname=IT',
     'username' => 'itapp',
-    'password' => 'wheKnis7',
+    'password' => 'FoufimVo',
     'charset' => 'utf8',
 ];
