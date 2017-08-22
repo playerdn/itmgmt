4558d8c37dc0365f1ed5368c0237b15fb3b6a446
playerdn <shevchyk.aa@gmail.com>
2017-08-22 17:37:35 +0300

    Update db_ungg.php

--- config/db_ungg.php
+++ config/db_ungg.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=127.0.0.1;dbname=IT',
     'username' => 'itapp',
-    'password' => 'jodFadma',
+    'password' => 'jodFadma123',
     'charset' => 'utf8',
 ];
