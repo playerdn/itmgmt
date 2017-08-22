c9348249547415110f5e8df32a040a4067199090
Andrey Shevchik <shevchyk.aa@gmail.com>
2017-08-22 17:31:27 +0300

    gitigonre 2

--- config/db_ungg.php
+++ config/db_ungg.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=127.0.0.1;dbname=IT',
     'username' => 'itapp',
-    'password' => 'FoufimVo',
+    'password' => 'jodFadma',
     'charset' => 'utf8',
 ];
