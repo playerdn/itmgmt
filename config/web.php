be6e07ed29bfda20a3bdaba571c8a2889b919359
Andrey Shevchik <shevchyk.aa@gmail.com>
2017-08-22 18:16:18 +0300

    Add htaccess with rewriting rules Enable pretty URL Enable Gii

--- config/web.php
+++ config/web.php
@@ -39,14 +39,10 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
-            'rules' => [
-            ],
         ],
-        */
     ],
     'params' => $params,
 ];
@@ -63,8 +59,7 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
-        // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
+        'allowedIPs' => ['*'],
     ];
 }
 
