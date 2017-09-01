0694bf558f5afd3fa48950694a061cb0b3522fa4
Andrey Shevchik <shevchyk.aa@gmail.com>
2017-09-01 18:24:55 +0300

    Remove debug output.

--- rbac/VpnOwnerRule.php
+++ rbac/VpnOwnerRule.php
@@ -8,8 +8,6 @@ class VpnOwnerRule extends \yii\rbac\Rule {
     public $name='isVpnOwner';
     
     public function execute($user, $item, $params) {
-        file_put_contents('/opt/lampp/htdocs/itmdev/runtime/log.log', print_r($params,true), FILE_APPEND);
-        
         if(! isset($params['vpnId'])){
             return FALSE;
         }
